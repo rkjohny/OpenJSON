6c87da446b23a0ecd57090a9b4deddbc103ac8de
rkjohny <rkjohny@gmail.com>
2023-08-20 12:10:57 +0600

    type traits directory added

--- include/TypeTraits.h
+++ include/TypeTraits.h
@@ -1,14 +1,14 @@
 #ifndef OPEN_JSON_TYPETRAITS_H
 #define OPEN_JSON_TYPETRAITS_H
 
-#include "TypeTraitsChar.h"
-#include "TypeTraitsInteger.h"
-#include "TypeTraitsBool.h"
-#include "TypeTraitsString.h"
-#include "TypeTraitsEnum.h"
-#include "TypeTraitsPrimitive.h"
-#include "TypeTraitsDecimal.h"
-#include "TypeTraitsVector.h"
-#include "TypeTraitsClass.h"
+#include "type_traits/TypeTraitsChar.h"
+#include "type_traits/TypeTraitsInteger.h"
+#include "type_traits/TypeTraitsBool.h"
+#include "type_traits/TypeTraitsString.h"
+#include "type_traits/TypeTraitsEnum.h"
+#include "type_traits/TypeTraitsPrimitive.h"
+#include "type_traits/TypeTraitsDecimal.h"
+#include "type_traits/TypeTraitsVector.h"
+#include "type_traits/TypeTraitsClass.h"
 
 #endif //OPEN_JSON_TYPETRAITS_H
